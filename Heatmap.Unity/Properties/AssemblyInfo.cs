2e2f7487940e28ea5e8ba4d534d814636abf880a
asdfCYBER <asdfCYBER@outlook.com>
2021-11-05 14:50:23 +0100

    Restructuring, add Heatmap.Unity assembly

---
+++ Heatmap.Unity/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Heatmap.Unity")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Heatmap.Unity")]
+[assembly: AssemblyCopyright("Copyright © asdfCYBER 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("22b01ee8-f5fd-4775-b142-191f96969235")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
---
+++ Heatmap.Unity/SettingsPanelManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Heatmap.Unity
+{
+    /// <summary>
+    /// Unity component which holds references to objects
+    /// the main assembly needs to do things with
+    /// </summary>
+    [RequireComponent(typeof(RectTransform))]
+    public class SettingsPanelManager : MonoBehaviour
+    {
+        [SerializeField]
+        public GameObject CloseButton;
+
+        [SerializeField]
+        public GameObject ColormapDropdown;
+
+        [SerializeField]
+        public GameObject MeasuringPeriodInputField;
+
+        [SerializeField]
+        public GameObject BusynessMultiplierInputField;
+    }
+}
