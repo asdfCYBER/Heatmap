1b967d68126dda215459c943bf2a4efbc77d23ac
asdfCYBER <asdfCYBER@outlook.com>
2025-04-16 13:31:52 +0200

    Make settings panel draggable

---
+++ Heatmap.Unity/Draggable.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Logviewer.Unity
+{
+    [RequireComponent(typeof(RectTransform))]
+    public class Draggable : MonoBehaviour, IDragHandler
+    {
+        private RectTransform _panelTransform;
+
+        public void Start()
+        {
+            _panelTransform = GetComponent<RectTransform>();
+        }
+
+        public void OnDrag(PointerEventData data)
+        {
+            // Move gameobject the same amount as the mouse moved
+            _panelTransform.position += new Vector3(data.delta.x, data.delta.y, 0);
+        }
+    }
+}
